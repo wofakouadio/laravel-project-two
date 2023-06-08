e0dc6aa8922f955af4bb391c1c6755e74a30b2dd
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2023-06-08 16:45:30 +0000

    Update show.blade.php

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,9 +1,8 @@
 
-<x-layout-home>
-    <x-slot:header_one>
-        <x-header-one/>
-    </x-slot>
+<x-blog-body>
+
     <div class="container-fluid">
+{{--        @props('blog')--}}
         <!-- ============================================================== -->
         <!-- Start Page Content -->
         <!-- ============================================================== -->
@@ -11,16 +10,19 @@
             <div class="col-lg-8 col-md-8">
                 <div class="card">
                     <div class="card-header">
-                        <h2 class="text-uppercase">this is the first blog</h2>
+                        <h2 class="text-uppercase">{{$blog->title}}</h2>
                     </div>
                     <div class="card-body">
                         <div class="text-center p-20">
-                            <img src="{{asset('assets/images/big/img1.jpg')}}" alt="user" width="50%"/>
+                            <img src="{{asset('storage/'.$blog->img)}}" alt="user" width="50%"/>
                         </div>
-                        <p class="text-justify">Recteque litora dico placerat graece appareat hac malesuada docendi intellegat. Commodo repudiare iriure curae dictas option primis delectus tacimates. Tempor volutpat convenire quaestio putent appetere prodesset. Dicit sapien saperet commodo mus. Curabitur tale phasellus eros vitae elaboraret. Accusata dis elit sit suspendisse novum cetero eripuit varius. Malesuada iisque scripta consequat epicurei elitr. Dolorem consectetur nobis mollis possit. Dolor egestas fusce consectetuer regione odio nostra volumus pri. Massa definitionem donec fabulas eirmod viris. Taciti labores tritani posidonium dolorum mnesarchum habemus vis. Alia partiendo class sapientem prodesset quisque. Nulla nibh affert duo ceteros error per viverra. Facilis tristique sollicitudin oratio no atomorum delectus principes tacimates posuere. Propriae gloriatur salutatus eu nobis sanctus eu nihil. Recteque tale donec invenire nonumes vehicula. Euripidis voluptatum rutrum ridiculus inceptos animal. Brute sanctus dicit definiebas ludus tincidunt turpis labores fusce. Pretium atqui esse atomorum metus netus expetenda repudiare ligula. Vivamus mattis movet alia brute tamquam eget. Principes minim civibus tota montes facilisi. Fringilla molestie finibus faucibus sale. Vehicula percipit dicta tamquam pretium elit. Convenire legere omittantur prompta nullam purus ridiculus. Appetere sodales leo penatibus idque pertinacia semper cetero.</p>
+                        <p class="text-justify">{{$blog->content}}</p>
                         <div class="">
-                            <h6>Source : Heidy Kirlin</h6>
-                            <h6>Date : {{date('Y-m-d')}}</h6>
+                            <h6>Source : {{$blog->author_name}}</h6>
+                            <h6>Date : {{$blog->created_at}}</h6>
+                            <h6>Category : <a href="?/category={{$blog->category}}">{{$blog->category}}</a></h6>
+                            <h6>Tags : <x-tags :Tags="$blog->tags"/></h6>
+
                         </div>
                     </div>
                 </div>
@@ -34,5 +36,5 @@
             </div>
         </div>
     </div>
-</x-layout-home>
+</x-blog-body>
 
